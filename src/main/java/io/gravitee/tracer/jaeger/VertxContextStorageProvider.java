3a6cc615f6bbac9b577d20a650903033bfa9f911
NIVEDITA SINGH <niveditashenoy@gmail.com>
2025-02-11 00:08:15 +0530

    fix: added null checks to vertxCtx

--- src/main/java/io/gravitee/tracer/jaeger/VertxContextStorageProvider.java
+++ src/main/java/io/gravitee/tracer/jaeger/VertxContextStorageProvider.java
@@ -40,6 +40,7 @@ public class VertxContextStorageProvider implements ContextStorageProvider {
 
         public Scope attach(io.vertx.core.Context vertxCtx, Context toAttach) {
             Context current = vertxCtx == null ? null : vertxCtx.getLocal(ACTIVE_CONTEXT);
+
             if (current == toAttach) {
                 return Scope.noop();
             }
